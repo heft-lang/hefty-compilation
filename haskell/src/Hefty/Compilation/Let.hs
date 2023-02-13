c3f24d807530e00b59f6455d1188d6765b2a5172
Jaro Reinders <jaro.reinders@gmail.com>
2023-02-13 14:33:10 +0100

    Generalize Let

--- haskell/src/Hefty/Compilation/Let.hs
+++ haskell/src/Hefty/Compilation/Let.hs
@@ -4,10 +4,10 @@ module Hefty.Compilation.Let where
 import Hefty
 
 data Let c m a where
-  Let :: m (c Int) -> (c Int -> m (c Int)) -> Let c m (c Int)
+  Let :: m (c a) -> (c a -> m (c b)) -> Let c m (c b)
 
 instance HFunctor (Let c) where
   hmap f (Let m b) = Let (f m) (f . b)
 
-let' :: (Let c << h) => Hefty h (c Int) -> (c Int -> Hefty h (c Int)) -> Hefty h (c Int)
+let' :: (Let c << h) => Hefty h (c a) -> (c a -> Hefty h (c b)) -> Hefty h (c b)
 let' m f = Op (injH $ Let m f) Return
